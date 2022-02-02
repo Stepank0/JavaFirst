5ae0b611fb982b53b15a8ec5bcb7fc1ebf412bfc
Stepan <stepanswim@gmail.com>
2022-02-02 12:24:10 +0300

    compleat  fifth homework

---
+++ src/Homework/FifthHW/Employee.java
@@ -0,0 +1,37 @@
+package Homework.FifthHW;
+
+public class Employee {
+    private static final int year = 2021;
+
+    private String fullName;
+    private String position;
+    private String email;
+    private String phone;
+    private int salary;
+    private int age;
+
+    // конструктор класса
+    public Employee(String fullName, String position, String email, String phone, int salary, int age) {
+        this.fullName = fullName;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+    }
+
+
+    int getAge() {
+        return age;
+
+    }
+
+    String getInfo () {
+        return this.fullName + "\n" +
+                this.position + "\n" +
+                this.email + "\n" +
+                this.phone + "\n" +
+                this.salary + "$" + "\n" +
+                this.getAge();
+    }
+}
